49e6ce97eedd8174d1f208ca7fd247aa6fc94cb8
roylaw <roy@Weibins-MacBook-Pro.local>
2018-12-05 23:30:54 -0500

    Add: CRUD operation for typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,68 @@
 import "reflect-metadata";
 import { GraphQLServer } from "graphql-yoga";
-import { createConnection } from "typeorm";
+import { createConnection, getConnection } from "typeorm";
+import { ResolverMap } from "./types/Resolvers";
+import { User } from "./entity/User";
 // ... or using `require()`
 // const { GraphQLServer } = require('graphql-yoga')
 
 const typeDefs = `
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String!
+    age: Int!
+    email: String!
+  }
+
   type Query {
     hello(name: String): String!
+    user(id: Int!): User!
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(firstName: String!, lastName: String!, age: Int!, email: String!): User!
+    updateUser(id: Int!, firstName: String, lastname: String, age: Int, email: String): Boolean!
+    deleteUser(id: Int!): Boolean!
   }
 `;
 
-const resolvers = {
+const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: any) => `hello ${name || "World"}`
+    hello: (_: any, { name }: any) => `hello ${name || "World"}`,
+    user: (_, { id }) => User.findOne(id),
+    users: () => User.find()
+  },
+
+  Mutation: {
+    createUser: (_, args) => {
+      const user = User.create(args);
+      return user.save();
+    },
+    updateUser: (_, { id, ...args }) => {
+      try {
+        User.update(id, args);
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    deleteUser: async (_, { id }) => {
+      try {
+        await getConnection()
+          .createQueryBuilder()
+          .delete()
+          .from(User)
+          .where("id = :id", { id: id })
+          .execute();
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
   }
 };
 
